b835410a278d1dd3085df06035b503154c937c2c
slowran <15528235793@163.com>
2017-11-09 15:31:55 +0800

    version 0.3, complete notification fragment ui

--- app/src/main/java/org/client/bracelet/ui/NotificationFragment.java
+++ app/src/main/java/org/client/bracelet/ui/NotificationFragment.java
@@ -1,13 +1,24 @@
 package org.client.bracelet.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.beardedhen.androidbootstrap.AwesomeTextView;
+import com.beardedhen.androidbootstrap.BootstrapButton;
 
 import org.client.bracelet.R;
+import org.client.bracelet.entity.ApplicationManager;
+import org.client.bracelet.utils.ViewFindUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 李浩然
@@ -15,7 +26,9 @@ import org.client.bracelet.R;
  */
 
 public class NotificationFragment extends Fragment {
-    private String title;
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private NotificationAdapter mAdapter;
 
     private static NotificationFragment singleton;
 
@@ -29,14 +42,71 @@ public class NotificationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        title = "通知";
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fr_simple_card, container, false);
-        TextView card_title_tv = (TextView) v.findViewById(R.id.card_title_tv);
-        card_title_tv.setText(title);
+        View v;
+        if (ApplicationManager.getInstance().isLogin()) {
+            v = inflater.inflate(R.layout.fragment_notification, null);
+            initData();
+            mRecyclerView = ViewFindUtils.find(v, R.id.id_recyclerview);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mRecyclerView.setAdapter(mAdapter = new NotificationAdapter());
+
+        } else {
+            v = inflater.inflate(R.layout.fragment_nologin, null);
+            BootstrapButton toLoginBtn = ViewFindUtils.find(v, R.id.btn_to_login);
+            toLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setClass(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            });
+        }
         return v;
     }
+
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            mDatas.add(i + "");
+        }
+    }
+
+    class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MessageHoder> {
+
+        @Override
+        public MessageHoder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MessageHoder hoder = new MessageHoder(LayoutInflater.from(getActivity()).inflate(R.layout.notification_item, parent, false));
+            return hoder;
+        }
+
+        @Override
+        public void onBindViewHolder(MessageHoder holder, int position) {
+            holder.from.setText(mDatas.get(position));
+            holder.message.setText(mDatas.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+
+        class MessageHoder extends RecyclerView.ViewHolder {
+
+            AwesomeTextView from, message;
+
+            public MessageHoder(View itemView) {
+                super(itemView);
+                from = ViewFindUtils.find(itemView, R.id.from);
+                message = ViewFindUtils.find(itemView, R.id.message);
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/org/client/bracelet/ui/SettingFragment.java
+++ app/src/main/java/org/client/bracelet/ui/SettingFragment.java
@@ -92,7 +92,6 @@ public class SettingFragment extends Fragment {
             });
         }
 
-
         return v;
     }
 
